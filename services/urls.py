0fe0bc27956f9f21db34a35b7278e895232ba5b8
otabek <baqara163@gmail.com>
2024-10-17 19:23:01 +0500

    Filter for Commission member by region, filter for project by status and pagination for it added and some changes done base views

--- services/urls.py
+++ services/urls.py
@@ -7,12 +7,15 @@ urlpatterns = [
     path('banner/', BannerViewSet.as_view({'get': 'banner_list'}), name='banner'),
     path('region/', RegionViewSet.as_view({'get': 'region_list'}), name='region'),
     path('project/', ProjectViewSet.as_view({'get': 'projects_list'}), name='project'),
+    path('projects/', ProjectViewSet.as_view({'get': 'filter_by_query_param'}), name='projects'),
     path('news/', NewsViewSet.as_view({'get': 'news_list'}), name='news'),
     path('opinion/', OpinionViewSet.as_view({'post': 'create_opinion'}), name='opinion'),
 
     path('commission/', CommissionViewSet.as_view({'get': 'commission_member_list'}), name='commission_list'),
     path('commission/<int:pk>/', CommissionViewSet.as_view({'get': 'commission_member_detail'}),
          name='commission_detail'),
+    path('region_commission/<int:pk>/', CommissionViewSet.as_view({'get': 'commission_member_by_region'}),
+         name='region_commission'),
     path('commission/category/', CommissionViewSet.as_view({'get': 'commission_category_list'}),
          name='commission_category'),
 
--- services/views.py
+++ services/views.py
@@ -2,13 +2,15 @@ from rest_framework.viewsets import ViewSet
 from rest_framework import status
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
-
+from drf_yasg import openapi
+from .utils import get_projects_filter
+from django.db.models import Q
 from exceptions.error_messages import ErrorCodes
 from exceptions.exception import CustomApiException
 from .models import Banner, Region, CommissionCategory, CommissionMember, Projects, AppealMember, Appeal, News, Opinion
 from .serializers import (
     BannerSerializer, RegionSerializer, CommissionMemberSerializer, ProjectsSerializer, CommissionCategorySerializer,
-    AppealSerializer, AppealMemberSerializer, NewsSerializer, OpinionSerializer
+    AppealSerializer, AppealMemberSerializer, NewsSerializer, OpinionSerializer, ParamValidateSerializer
 )
 
 
@@ -64,6 +66,20 @@ class CommissionViewSet(ViewSet):
         return Response(data={'result': serializer.data, 'ok': True}, status=status.HTTP_200_OK)
 
     @swagger_auto_schema(
+        operation_summary='Commission members by region',
+        operation_description='List of commission members by region',
+        responses={200: CommissionMemberSerializer()},
+        tags=['Commission']
+    )
+    def commission_member_by_region(self, request, pk):
+        commission_members = CommissionMember.objects.filter(region_id=pk)
+        if not commission_members:
+            raise CustomApiException(ErrorCodes.NOT_FOUND, message='Commission members not found')
+        return Response(
+            data={'result': CommissionMemberSerializer(commission_members, many=True, context={'request': request}).data,
+                  'ok': True})
+
+    @swagger_auto_schema(
         operation_summary='List Of Commission Categories',
         operation_description='List of commission categories',
         responses={200: CommissionCategorySerializer()},
@@ -86,6 +102,38 @@ class ProjectViewSet(ViewSet):
         projects = Projects.objects.all()
         serializer = ProjectsSerializer(projects, many=True, context={'request': request})
         return Response(data={'result': serializer.data, 'ok': True}, status=status.HTTP_200_OK)
+
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                name='page', in_=openapi.IN_QUERY, description='Page number', type=openapi.TYPE_INTEGER),
+            openapi.Parameter(
+                name='page_size', in_=openapi.IN_QUERY, description='Page size', type=openapi.TYPE_INTEGER),
+            openapi.Parameter(
+                name='status', in_=openapi.IN_QUERY, description='Status', type=openapi.TYPE_INTEGER)
+        ],
+        operation_summary='List Of Projects by Type',
+        operation_description='List of projects by type',
+        responses={200: ProjectsSerializer()},
+        tags=['Project']
+    )
+    def filter_by_query_param(self, request):
+        serializer = ParamValidateSerializer(data=request.query_params, context={'request': request})
+        if not serializer.is_valid():
+            raise CustomApiException(ErrorCodes.VALIDATION_FAILED, serializer.errors)
+        status_ = serializer.validated_data.get('status')
+        page_size = serializer.validated_data.get('page_size')
+        page = serializer.validated_data.get('page')
+        query = Q()
+        if status_:
+            query &= Q(status=status_)
+        # if status_:
+        projects = Projects.objects.filter(query).order_by('id')
+        # else:
+        #     projects = Projects.objects.all().order_by('id')
+        response = get_projects_filter(context={'request': request, 'project_param': projects}, page=page, page_size=page_size)
+        return Response(data={'result': response, 'ok': True}, status=status.HTTP_200_OK)
 
 
 class AppealViewSet(ViewSet):
