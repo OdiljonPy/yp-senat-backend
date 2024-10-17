11aa7dbf1571c911443a2276bfa407fbb9926cbf
abbosbek <hikmatullaevabbos24@gmail.com>
2024-10-17 15:46:31 -0400

    cors headers settings added

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
     # packages
     'rest_framework',
+    'corsheaders',
     'drf_yasg',
     'django_ckeditor_5',
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -70,8 +72,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
     'x-api-key',
-    'content-type',
-    'Authorization'
+    'content-type'
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
