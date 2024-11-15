18783b0eb98c108b6cb6aa1aee1e3a5a75ff412b
Odiljon Abduvaitov <73192064+OdiljonPy@users.noreply.github.com>
2024-11-16 04:14:22 +0500

    Update dev.py

--- config/gunicorn/dev.py
+++ config/gunicorn/dev.py
@@ -7,5 +7,5 @@ bind = '0.0.0.0:8007'
 reload = False
 accesslog = errorlog = f'/var/log/gunicorn/{project_name}-dev.log'
 capture_output = True
-pidfile = f'/var/run/gunicorn/{project_name}-dev.pid'
+# pidfile = f'/var/run/gunicorn/{project_name}-dev.pid'
 # daemon = True
