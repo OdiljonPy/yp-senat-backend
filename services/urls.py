a7017cb8f244383487936294fc928ed510dad524
suxrob <suxrobergashev2102@gmail.com>
2024-11-28 01:44:36 -0500

    posts

--- services/urls.py
+++ services/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('projects/', ProjectViewSet.as_view({'get': 'filter_by_query_param'}), name='projects'),
     # path('post/member/', PostViewSet.as_view({'get': 'post_list_by_members'}), name='post_by_member'),
     path('post/<int:pk>/', PostViewSet.as_view({'get': "post_detail"}), name='post_detail'),
-    path('post/category/', PostViewSet.as_view({'get': 'post_list_by_category'}), name='posts_by_category_id'),
+    path('posts/', PostViewSet.as_view({'get': 'post_list_by_category'}), name='posts_by_category_id'),
     path('categories/', CategoryViewSet.as_view({'get': 'list'}), name='category_list'),
     path('banner/', PostViewSet.as_view({'get': 'banner'}), name='banner'),
 
